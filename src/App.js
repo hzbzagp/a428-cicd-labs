b3979a715b6f1d1dc541517c96bdfd0276a8b73a
hzbzagp <137499376+hzbzagp@users.noreply.github.com>
2023-09-23 13:27:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Fuckin   React!!!!</h1>
+          <h1 className="App-title">Example Of React APP!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src='https://picsum.photos/id/237/200/300' />
       </div>
     );
   }
